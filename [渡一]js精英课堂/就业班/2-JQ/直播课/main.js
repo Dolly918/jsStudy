b7bdf495c98d3fa2497e8d3c4b8f412935344fa0
xing.org1^ <xing.org1@outlook.com>
2019-02-12 11:53:32 +0800

    jq版本轮播图封装完毕

--- [渡一]js精英课堂/就业班/2-JQ/直播课/main.js
+++ [渡一]js精英课堂/就业班/2-JQ/直播课/main.js
@@ -2,9 +2,12 @@
  * @Author: @Guojufeng 
  * @Date: 2019-02-11 20:29:10 
  * @Last Modified by: @Guojufeng
- * @Last Modified time: 2019-02-11 21:57:48
+ * @Last Modified time: 2019-02-12 11:40:09
  */
 $('.wrapper').swiper({
-  imgList: ['1.img','2.img','3.img'],
+  imgList: ['./img/1.jpg','./img/2.jpg','./img/3.jpg','./img/2.jpg','./img/4.jpg','./img/5.jpg'],
+  imgWidth: 600,
+  duration: 2000,
+  type: 'marquee',
   showBtn: true
 })
