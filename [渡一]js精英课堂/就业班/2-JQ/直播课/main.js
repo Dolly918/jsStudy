92acaa9e6f899e7b4652f4d8779d36327fa95a68
xing.org1^ <xing.org1@outlook.com>
2019-02-13 08:46:38 +0800

    拖拽原理分析

--- [渡一]js精英课堂/就业班/2-JQ/直播课/main.js
+++ [渡一]js精英课堂/就业班/2-JQ/直播课/main.js
@@ -2,7 +2,7 @@
  * @Author: @Guojufeng 
  * @Date: 2019-02-11 20:29:10 
  * @Last Modified by: @Guojufeng
- * @Last Modified time: 2019-02-11 21:57:48
+ * @Last Modified time: 2019-02-12 20:38:44
  */
 $('.wrapper').swiper({
   imgList: ['1.img','2.img','3.img'],
